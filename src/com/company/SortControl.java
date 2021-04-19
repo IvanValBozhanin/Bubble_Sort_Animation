c95824316a952b29948b6385585c85f11979ebcb
Ivan Valentinov <ivan156drs@gmail.com>
2021-04-19 09:10:19 +0300

    Updated SortControl file.

--- src/com/company/SortControl.java
+++ src/com/company/SortControl.java
@@ -2,21 +2,18 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class SortControl extends JPanel {
     private final SortingPanel sortingPanel = new SortingPanel();
-
-    private JButton nextStepBtn = new JButton("NextStep");
 
     public SortControl() {
 
         setLayout(new BorderLayout());
         add(sortingPanel, BorderLayout.CENTER);
+        JButton nextStepBtn = new JButton("NextStep");
         add(nextStepBtn, BorderLayout.SOUTH);
 
         nextStepBtn.addActionListener(arg0 -> sortingPanel.nextStepInSorting());
-
     }
 
 }
